180e9cab1faae3cbb4f0185f60fbd357e233e684
Willy Vasquez <wvasquez@xumak.com>
2022-01-06 20:19:04 -0600

    adding style

--- src/App.js
+++ src/App.js
@@ -2,24 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+      <h1>My To-do's</h1>
+      <div class='card' >
+          <h2>Title!</h2>
+          <div>
+            <button>Delete</button>
+          </div>
+      </div>
+  </div>;
 }
 
 export default App;
